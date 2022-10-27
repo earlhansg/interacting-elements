68f7b5e5db2cd178e81f5cbf94197bf8b7e90342
Earl Hans Genoso <earlhansg@gmail.com>
2022-10-28 04:13:53 +0800

    clicking button by css selector

---
+++ src/ClickingButtons.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ClickingButtons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\driver_v1\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.automationtesting.co.uk/buttons.html");
+		
+		driver.findElement(By.cssSelector("button#btn_three")).click();
+
+	}
+
+}
