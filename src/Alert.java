bd3888cf2da0bf93db6a619821a26675237dab45
Earl Hans Genoso <earlhansg@gmail.com>
2022-10-29 20:21:42 +0800

    click button that triggers Alert and accept it

---
+++ src/Alert.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alert {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\EarlHans Geñoso\\Desktop\\Resources\\driver_v1\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://www.automationtesting.co.uk/popups.html");
+		driver.findElement(By.cssSelector("[onclick='alertTrigger\\(\\)']")).click();
+		Thread.sleep(3000);
+		driver.switchTo().alert().accept();
+
+	}
+
+}
